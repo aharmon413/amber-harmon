0a4e733a0f0d96149500a544eedd73d07bd1d73f
Amber Harmon <amber@amberharmon.com>
2022-02-04 18:23:57 -0500

    Site redesign

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
 const toggle = document.querySelector('#menu-toggle');
 const menu = document.querySelector('nav');
 
+let typed = new TypeIt('#typed', {
+    strings: [`{ 💻 }`,
+    '{ ☕ }',
+    '{ 📚 }',
+    '{ ✨ }',
+    '{ 🎧 }',
+    '{ 📂 }',
+    '{ 📱 }',
+    '{ 🌱 }',
+    '{ 💫 }',
+    '{ 📋 }',
+    '{ 💾 }',
+    '{ 💕 }'],
+    speed: 100,
+    startDelay: 250,
+    nextStringDelay: 3000,
+    breakLines: false,
+    loop: true
+}).go();
+
+function openMenu() {
+    menu.classList.add('active');
+    toggle.classList.replace('fa-bars', 'fa-times');
+    toggle.setAttribute('aria-expanded', 'true');
+    toggle.setAttribute('aria-label', 'Close navigation menu');
+}
+
+function closeMenu() {
+    menu.classList.remove('active');
+    toggle.classList.replace('fa-times', 'fa-bars');
+    toggle.setAttribute('aria-expanded', 'false');
+    toggle.setAttribute('aria-label', 'Open navigation menu');
+}
+
 // close hamburger menu after a navigation link is clicked
-document.querySelectorAll('.navMenu li a').forEach(link => 
+document.querySelectorAll('.nav-menu li a').forEach(link => 
     link.addEventListener('click', function() {
         closeMenu();
     })
@@ -17,14 +51,4 @@ toggle.addEventListener('click', function() {
     }
 });
 
-function openMenu() {
-    menu.classList.add('active');
-    toggle.classList.replace('fa-bars', 'fa-times');
-    toggle.setAttribute('aria-expanded', 'true');
-}
-
-function closeMenu() {
-    menu.classList.remove('active');
-    toggle.classList.replace('fa-times', 'fa-bars');
-    toggle.setAttribute('aria-expanded', 'false');
-}
+window.addEventListener('resize', () => { if (window.innerWidth > 765) closeMenu()} );
